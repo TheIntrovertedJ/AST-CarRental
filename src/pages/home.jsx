079cfb69aa6058981ef7e7d2c59ef9500229c995
Japhet <daisioluwatobiloba441@gmail.com>
2026-09-17 17:08:51 +0100

    Add Tailwind Vite plugin

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,17 @@
+import Banner from '../components/Banner';
+import FeaturedSection from '../components/FeaturedSection';
 import Hero from '../components/hero';
+import Newsletter from '../components/Newsletter';
+import Testimonial from '../components/Testimonial';
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
+			<FeaturedSection />
+			<Banner />
+			<Testimonial />
+			<Newsletter />
 		</>
 	);
 };
